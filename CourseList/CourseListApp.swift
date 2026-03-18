4be806752bcf4714f956edca576780a96fc01cf1
libr <me@nvme0n1p.dev>
2026-03-18 19:22:00 +0800

    Initial Commit

---
+++ CourseList/CourseListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseListApp.swift
+//  CourseList
+//
+//  Created by LiBr on 18/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct CourseListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
